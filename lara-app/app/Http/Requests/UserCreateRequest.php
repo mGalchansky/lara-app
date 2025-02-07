f70a4e42dfc71b0511c9f31b42a87d5b5cfeef8c
Mykhailo <mgalchansky@gmail.com>
2025-02-08 00:58:46 +0200

    register

---
+++ lara-app/app/Http/Requests/UserCreateRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Rules\PhoneNumber;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserCreateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:35'],
+            'lastname' => ['required', 'string', 'max:50'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'phone' => ['required', 'string', 'min:10', 'max:15', new PhoneNumber],
+        ];
+    }
+}
